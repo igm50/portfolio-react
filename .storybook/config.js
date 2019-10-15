d67a0e1bb87fbf6e96f9313d93b05798c67d7f62
kigimi-hrs <kigimi@hr-s.co.jp>
2019-10-15 12:54:33 +0900

    プロジェクト初期化

---
+++ .storybook/config.js
@@ -0,0 +1,35 @@
+import { addParameters, configure } from '@storybook/react';
+import requireContext from 'require-context.macro'
+
+const categories = [
+  'atoms',
+  'molecules',
+  'organisms',
+  'ecosystems',
+  'environments'
+]
+const categoryNumber = id => {
+  const categoryName = id.split('-')[0]
+  return categories.indexOf(categoryName)
+}
+
+const compareCategory = (a, b) => {
+  const aNumber = categoryNumber(a[1].id)
+  const bNumber = categoryNumber(b[1].id)
+  return aNumber.toString().localeCompare(bNumber.toString(), { numeric: true })
+}
+
+const compareComponent = (a, b) => {
+  return a[1].id.localeCompare(b[1].id)
+}
+
+addParameters({
+  options: {
+    storySort: (a, b) => {
+      const result = compareCategory(a, b)
+      return result != 0 ? result : compareComponent(a, b)
+    }
+  }
+})
+
+configure(requireContext('../src', true, /\.stories\.tsx$/), module);
